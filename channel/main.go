d024611c9e7cfea25e8b0b97498637beddaba6d9
Fajla Rabby <fajlarabbyla24@gmail.com>
2026-05-16 00:12:04 +0600

    introduce channel and un-buffered call

---
+++ channel/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(1)
+	var ch = make(chan string) // un-buffered channel
+	go uploadFile(ch)
+
+	wg.Wait()
+	// fileUrl := <-ch // * blocking call -> wait for file url
+	// fmt.Println("Ch", ch)
+	// fmt.Println("File url", fileUrl)
+}
+
+func uploadFile(c chan string) {
+	defer wg.Done()
+
+	fmt.Println("Uploading file...")
+	time.Sleep(3 * time.Second)
+	fmt.Println("File uploaded done")
+	fileUrl := "https://cloudinary.com"
+	c <- fileUrl
+
+}
